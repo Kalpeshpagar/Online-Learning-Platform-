5e7538d63239dea1342f25d18eba303f13a6a601
Kalpesh Pagar <kalpeshpagar57@gmail.com>
2025-08-09 23:16:24 +0530

    TumelineSection and InstructorSection page also done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,13 @@ import HighlightText from '../components/core/HomePage/HighlightText';
 import CTAButton from '../components/core/HomePage/Button';
 import Banner from '../assets/Images/banner.mp4';
 import CodeBlocks from '../components/core/HomePage/CodeBlocks';
+import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection';  
+import TimelineSection from '../components/core/HomePage/TimelineSection';
+import InstructorSection from '../components/core/HomePage/InstructorSection';
+import '../App.css'; // Ensure to import your CSS file for styles
+// import ReviewSlider from '../components/core/HomePage/ReviewSlider';
+import Footer from '../components/common/Footer';
+import ExploreMore from '../components/core/HomePage/ExploreMore';
 
 function Home() {
   return (
@@ -109,15 +116,71 @@ function Home() {
               active: false,
             }}
             codeColor={"text-white"}
-            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+            codeblock={`<!DOCTYPE html>\n <html lang="en">\n<head>\n<title>This is myPage</title>\n</head>\n<body>\n<h1><a href="/">Header</a></h1>\n<nav> <a href="/one">One</a> <a href="/two">Two</a> <a href="/three">Three</a>\n</nav>\n</body>`}
             backgroundGradient={<div className="codeblock2 absolute"></div>}
           />
               </div>
-              
+            {/* Explore Section */}
+        <ExploreMore />
           </div>
       {/* Section 2 */}
+      <div className="bg-pure-greys-5 text-richblack-700">
+        <div className="homepage_bg h-[320px]">
+          {/* Explore Full Catagory Section */}
+          <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8">
+            <div className="lg:h-[150px]"></div>
+            <div className="flex flex-row gap-7 text-white lg:mt-8">
+              <CTAButton active={true} linkto={"/signup"}>
+                <div className="flex items-center gap-2">
+                  Explore Full Catalog
+                  <FaArrowRight />
+                </div>
+              </CTAButton>
+              <CTAButton active={false} linkto={"/login"}>
+                Learn More
+              </CTAButton>
+            </div>
+          </div>
+        </div>
+
+        <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 ">
+          {/* Job that is in Demand - Section 1 */}
+          <div className="mb-10 mt-[-100px] flex flex-col justify-between gap-7 lg:mt-20 lg:flex-row lg:gap-0">
+            <div className="text-4xl font-semibold lg:w-[45%] ">
+              Get the skills you need for a{" "}
+              <HighlightText text={"job that is in demand."} />
+            </div>
+            <div className="flex flex-col items-start gap-10 lg:w-[40%]">
+              <div className="text-[16px]">
+                The modern educational landscape dictates its own terms. Today, being a competitive specialist requires more than just professional skills.
+              </div>
+              <CTAButton active={true} linkto={"/signup"}>
+                <div className="">Learn More</div>
+              </CTAButton>
+            </div>
+          </div>
+          
+          {/* Timeline Section - Section 2 */}
+          <TimelineSection />
+
+          {/* Learning Language Section - Section 3 */}
+          <LearningLanguageSection />
+        </div>
+      </div>
       {/* Section 3 */}
+       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
+        {/* Become a instructor section */}
+        <InstructorSection />
+
+        {/* Reviws from Other Learner */}
+        {/* <h1 className="text-center text-4xl font-semibold mt-8">
+          Reviews From Other Learner
+        </h1>
+        <ReviewSlider /> */}
+      </div>
+
       {/* Footer */}
+      <Footer />
 
     </div>
   )
